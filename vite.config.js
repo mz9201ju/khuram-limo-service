24c47543787d6918b7ad36d2e3f32bdc2637f56f
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2025-11-08 17:05:59 -0800

    reverted base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/khuram-limo-service/',   // <--- add this
+  base: '/',   // <--- add this
   plugins: [react()],
   build: {
     assetsDir: 'assets',
